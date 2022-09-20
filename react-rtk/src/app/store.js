2d63c9513165b40896e79ec2abc40166e8c8f367
Boram Kim <boram2445@gmail.com>
2022-09-20 15:41:53 +0900

    [9월 20일] react에 react toolkit 사용

---
+++ react-rtk/src/app/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+// const reduxLogger = require("redux-logger");
+import cakeReducer from "../features/cake/cakeSlice";
+import icecreamReducer from "../features/icecream/icecreamSlice";
+import userReduer from "../features/user/userSlice";
+
+// const logger = reduxLogger.createLogger();
+
+const store = configureStore({
+  reducer: {
+    cake: cakeReducer,
+    icecream: icecreamReducer,
+    user: userReduer,
+  },
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+export default store;
---
+++ react-rtk/src/features/icecream/IcecreamView.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ordered, restocked } from "./icecreamSlice";
+
+export const IcecreamView = () => {
+  const [value, setValue] = useState(1);
+  const numOfIcecream = useSelector((state) => state.icecream.numOfIcecream);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>Number of ice creams - {numOfIcecream}</h2>
+      <button onClick={() => dispatch(ordered())}>Order ice cream</button>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+      />
+      <button onClick={() => dispatch(restocked(value))}>
+        Restock ice cream
+      </button>
+    </div>
+  );
+};
