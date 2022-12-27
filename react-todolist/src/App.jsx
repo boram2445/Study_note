83ae99c188420cf06cf91ce96c50e14cd17f8722
Boram Kim <boram2445@gmail.com>
2022-12-27 15:12:05 +0900

    [12월 27일] todo list yarn berry setting

---
+++ react-todolist/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Items from "./components/Items";
+
+export default function App() {
+  return (
+    <div>
+      <Items />
+    </div>
+  );
+}
---
+++ react-todolist/src/components/Items.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Item from "./Item";
+import { BsTrash } from "react-icons/bs";
+
+export default function Items() {
+  return (
+    <div>
+      <Item />
+      <BsTrash />
+    </div>
+  );
+}
