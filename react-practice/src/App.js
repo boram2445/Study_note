1be3dcf0e41cf4b0e212aac50a31e111f911c03e
Boram Kim <boram2445@gmail.com>
2023-01-01 21:41:09 +0900

    [1월 1일] 리액트 라우터 이해하기

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,17 +1,25 @@
-import Button1 from "./components/Button1";
-import Button2 from "./components/Button2";
-import StyledComponent from "./StyledComponent";
-import TailwindComponent from "./TailwindComponent";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./components/NotFound";
+import Home from "./components/Home";
+import Videos from "./components/Videos";
+import Root from "./components/Root";
+import VideoDetail from "./components/VideoDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/videos", element: <Videos /> },
+      { path: "/videos/:videoId", element: <VideoDetail /> },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <Button1 />
-      <Button2 />
-      <StyledComponent />
-      <TailwindComponent />
-    </>
-  );
+  return <RouterProvider router={router}>App</RouterProvider>;
 }
 
 export default App;
