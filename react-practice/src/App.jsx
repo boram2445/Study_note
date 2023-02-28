217ca6abd609d227929cb7abf4af032cff7604ec
Boram Kim <boram2445@gmail.com>
2023-02-28 20:01:39 +0900

    [2월 28일] 리액트 ts 프로젝트 셋팅

---
+++ react-practice/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hi there</h1>
+      <p>This is new!</p>
+    </div>
+  );
+}
+
+export default App;
--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import AppReactQuery from "./AppReactQuery";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppReactQuery />
+    <App />
   </React.StrictMode>
 );
